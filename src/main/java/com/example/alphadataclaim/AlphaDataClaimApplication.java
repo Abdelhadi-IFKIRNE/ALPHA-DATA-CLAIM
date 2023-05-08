579479cb5a318c7137b4acb0dd81a241ec8d275a
Abdelhadi-IFKIRNE <abdelhadi.ifkirne@gmail.com>
2023-05-08 16:35:36 +0000

    update ...

---
+++ src/main/java/com/example/alphadataclaim/AlphaDataClaimApplication.java
@@ -0,0 +1,13 @@
+package com.example.alphadataclaim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaDataClaimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaDataClaimApplication.class, args);
+    }
+
+}
